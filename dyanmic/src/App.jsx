7048b3db905bf77f70069663adf8ebf768ac114a
deepak01112002 <deepak8574757609@gmail.com>
2025-01-21 17:44:54 +0530

    add

--- dyanmic/src/App.jsx
+++ dyanmic/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import BasicExample from './Component/Acco'
 
 function App() {
   
   const [state,setState] = useState({
     name : "",
     email : "",
-    skill : [""]
+    skill : [""],
+    hobbies : [""]
   })
   const handleadd = ()=>{
      setState({
@@ -19,32 +21,45 @@ function App() {
 
   const handleChange = (e)=>{
     const {name,value} = e.target
-    if(isNaN(name)){
+    // if(isNaN(name)){
     setState({...state,[name] : value})
-    }else{
-       let arr = [...state.skill]
-       arr[name] = value
-       setState({...state,skill : arr})
-    }
+    // }else{
+    //    let arr = [...state.skill]
+    //    arr[name] = value
+    //    setState({...state,skill : arr})
+    // }
   }
-
+  const handleDelete = (i)=>{
+    let arr = [...state.skill]
+    arr.splice(i,1)
+    setState({...state,skill : arr})
+    
+  }
+ 
+  const handleSkill = (e,index)=>{
+     let arr = [...state.skill]
+     arr[index] = e.target.value
+     setState({...state,skill : arr})
+     
+  }
   console.log(state);
-  
   return (
     <>
       <form action="">
          <input type="text" placeholder='Name' name='name'  onChange={handleChange}/>
          <input type="text" placeholder='Email' name='email'  onChange={handleChange}/>
-        <input type='button'value={"add"} onClick={handleadd}/>
+        <input type='button' className='btn btn-secondary' value={"add"} onClick={handleadd}/>
         {
           state.skill.map((el,index)=>{
             return <>
-                <input  type='text' name={index} placeholder='Skills' onChange={handleChange}/>
-                <input type="button" value={"delete"} />
+                <input  type='text' name="skill" placeholder='Skills' onChange={(e)=>handleSkill(e,index)}/>
+               {state.skill.length > 1 ? <input type="button" onClick={()=>handleDelete(index)} value={"del"} /> : ""}
             </>
           })
         }
+        
       </form>
+      <BasicExample/>
     </>
   )
 }
